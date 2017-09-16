e1afbac325880c3ecc5e31d4059905b370dcc2eb
tony <totoaj@gmail.com>
2017-09-16 14:17:19 -0700

    commit new changes

--- MyFirstProject/src/DateTest.java
+++ MyFirstProject/src/DateTest.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class DateTest {
 	public static void main(String[] args) {
 
-		// Create an array list.
+		// Create an array list. Git repository
 		 ArrayList<String> al = new ArrayList<String>();
 		 // Add elements to the array list.
 		 al.add("C");
